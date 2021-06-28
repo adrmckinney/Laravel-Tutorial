5d68a5a4d8c28b7eb0287f9926c6dbdec45e9821
Daniel McKinney <adrmckinney@gmail.com>
2021-06-28 11:32:04 -0400

    installed vue

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, '
 Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
 Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
