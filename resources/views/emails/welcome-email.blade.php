e3bf8c410d8714924415ff00b11a780a66baeab4
Daniel McKinney <adrmckinney@gmail.com>
2021-06-29 03:09:21 -0400

    almost got mailtrap working on registration. Error Class AppModelsEmail not found. Not sure why it's looking in Models for this. I'll take another look in the morning.

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Thanks for registering!
+
+This is my first Laravel app to build. This was a lot of fun. I do hope that the next app goes more smoothly.
+
+
+
+Best,<br>
+Dan
+@endcomponent
