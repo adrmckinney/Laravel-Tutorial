97380a5efce007222d06c879ef932cd097e23b81
Daniel McKinney <adrmckinney@gmail.com>
2021-06-29 00:22:20 -0400

    following and unfollowing works. Button toggles.

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(User $user)
     {
-        return $user->id;
-        // return auth()->user()->following()->toggle($user->profile);
+        return auth()->user()->following()->toggle($user->profile);
     }
 }
